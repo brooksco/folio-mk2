18213a8623d1c98e032ee0842c8bb127151c5a9d
Colin Brooks <colinbooks@gmail.com>
2013-10-10 10:23:55 +1300

    minor visual changes

--- couch/config.php
+++ couch/config.php
@@ -45,7 +45,7 @@
     define( 'K_DB_USER', 'root' );
     // 6.
     // Database password 
-    define( 'K_DB_PASSWORD', '' );
+    define( 'K_DB_PASSWORD', 'password' );
     // 7.
     // MySQL hostname (it will usually be 'localhost' )
     define( 'K_DB_HOST', 'localhost' );
